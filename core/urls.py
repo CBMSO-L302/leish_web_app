e58d2a364255bb7561f9f7579cca2bad46081caf
rfpacheco <ronnyfph@gmail.com>
2025-03-06 16:33:35 +0100

    Simplify template structure and update URL routing.
    Replaced function-based view with a class-based view for the homepage in `urls.py`. Refactored the template to use Django's template inheritance, improving maintainability and reducing redundancy. This ensures cleaner and more efficient template structure.

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import HomePageView
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", HomePageView.as_view(), name="index"),
 ]
