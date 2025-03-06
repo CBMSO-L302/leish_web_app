f5a19bcda16c7073643b846c0c616c81393294ce
rfpacheco <ronnyfph@gmail.com>
2025-03-06 14:42:04 +0100

    Add initial Django core app with basic structure and index view
    Created a new "core" app with models, views, templates, and URLs. Integrated the app into project settings and routed the root URL to the core app's index view. Added a basic HTML structure for the homepage.

--- leish_web_app/settings.py
+++ leish_web_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "core",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -54,7 +55,9 @@ ROOT_URLCONF = "leish_web_app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "template"
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
