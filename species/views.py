cdb5671dcc13931a626573a170bbaeafa5498116
rfpacheco <ronnyfph@gmail.com>
2025-03-19 16:02:26 +0100

    Add species pages, views, and URLs for Leishmania infantum general page
    Implemented individual templates, static resources, and views for four Leishmania species (infantum, major, donovani, braziliensis). Updated URL routing and navigation to integrate these pages into the app. Included CSS for styling and added relevant images for each species.

--- species/views.py
+++ species/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class SpeciesInfantumView(TemplateView):
+    template_name = "species/infantum.html"
+
+class SpeciesMajorView(TemplateView):
+    template_name = "species/major.html"
+
+class SpeciesDonovaniView(TemplateView):
+    template_name = "species/donovani.html"
+
+class SpeciesBraziliensisView(TemplateView):
+    template_name = "species/braziliensis.html"
