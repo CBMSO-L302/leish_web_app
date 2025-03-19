88c3a3607c8a001f5149f222635d7f1f8454f2e0
rfpacheco <ronnyfph@gmail.com>
2025-03-19 11:08:11 +0100

    Add team carousel functionality with dynamic image loading
    Introduce a JavaScript-based image carousel to the team section, fetching images from a new JSON file. Updated the base HTML template to include dynamic script blocks and configured static file settings for production. Added related image assets and removed unused/obsolete scripts and comments for cleaner code.

--- leish_web_app/settings.py
+++ leish_web_app/settings.py
@@ -124,6 +124,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+# Directory where `collectstatic` will store files in production
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
