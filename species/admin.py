4c819c262b445114d50eaca56a97eb7a49856ab3
ronnyfph <ronnyfph@gmail.com>
2025-03-24 13:11:56 +0100

    Add admin interface for Infantum model
    Registered the Infantum model in the Django admin panel. Configured list display and search fields for improved usability and management.

--- species/admin.py
+++ species/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Infantum
+
 # Register your models here.
+@admin.register(Infantum)
+class InfantumAdmin(admin.ModelAdmin):
+    list_display = ('id', 'gene_id', 'name', 'other_names', 'wikidata', 'mendeley', 'uniprot', 'lmjf_ortholog', 'lmjfc_ortholog', 'ldhu3_ortholog', 'lbrm2904_ortholog', )
+    search_fields = ('gene_id', 'name', 'other_names', )
+
+
+
+
+
