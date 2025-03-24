d4b99fc74cc530e6bc359aa00cbb190047d32e55
ronnyfph <ronnyfph@gmail.com>
2025-03-24 12:44:31 +0100

    Fix typo in template and ensure URL patterns consistency
    Corrected an ID typo in the base.html template affecting the "Team" link and ensured proper formatting in the URLs configuration file to align with coding standards. Also, added a new database file related to 'leishmania_BK'.

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from .views import HomePageView, OnDevelopment
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("on-development", OnDevelopment.as_view(), name="on-development"),
+    path("on-development", OnDevelopment.as_view(), name="on-development")
 ]
