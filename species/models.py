570cdd439677dd74c5dae99aacebb64e081040c6
ronnyfph <ronnyfph@gmail.com>
2025-03-24 11:24:55 +0100

    Add Infantum model and database router for Leishmania
    This commit introduces the `Infantum` model with comprehensive fields and links it to the `leishmania` database via a custom database router. The database router ensures reads and writes for species models are directed correctly and prevents migrations on the `leishmania` database. Additionally, a new initial migration is added for the updated `Infantum` model structure.

--- species/models.py
+++ species/models.py
@@ -7,32 +7,20 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-
-class Braziliensis(models.Model):
-    cdsid = models.TextField(blank=True, null=True)
-    ortholog = models.TextField(blank=True, null=True)
-    associatedfunction = models.TextField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'braziliensis'
-
-
-class Donovani(models.Model):
-    cdsid = models.TextField(blank=True, null=True)
-    ortholog = models.TextField(blank=True, null=True)
-    associatedfunction = models.TextField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'donovani'
-
-
 class Infantum(models.Model):
-    pass
-
-    def __str__(self):
-        return self.gene_id
+    id = models.AutoField(primary_key=True)  # Add an auto-incrementing primary key
+    gene_id = models.CharField(db_column='Gene_ID', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    name = models.CharField(db_column='Name', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    other_names = models.CharField(db_column='Other_names', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    wikidata = models.CharField(db_column='Wikidata', max_length=200, blank=True, null=True)  # Field name made lowercase.
+    mendeley = models.CharField(db_column='Mendeley', max_length=200, blank=True, null=True)  # Field name made lowercase.
+    uniprot = models.CharField(db_column='UniProt', max_length=200, blank=True, null=True)  # Field name made lowercase.
+    lmjf_ortholog = models.CharField(db_column='LmjF_ortholog', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    lmjfc_ortholog = models.CharField(db_column='LmjFC_ortholog', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    ldhu3_ortholog = models.CharField(db_column='LdHU3_ortholog', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    lbrm2904_ortholog = models.CharField(db_column='LBRM2904_ortholog', max_length=255, blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
+        managed = False
         db_table = 'infantum'
-        managed = True
 
