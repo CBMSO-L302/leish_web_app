8f2306aee4bcc97a2a43627689fb0fa05ff66086
ronnyfph <ronnyfph@gmail.com>
2025-03-24 12:24:26 +0100

    Add "On Development" page with route and template
    Created a new "On Development" page with a corresponding template, view, and route. Updated the navigation links for "Mission" and "Team" to point to the new page. This serves as a placeholder for content under development.

--- home/views.py
+++ home/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home/home.html"
+
+class OnDevelopment(TemplateView):
+    template_name = "home/on_development.html"
