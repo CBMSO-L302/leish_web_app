4b55356ca4e6786652f113fa859e232c573cb0de
ronnyfph <ronnyfph@gmail.com>
2025-04-22 20:25:50 +0200

    Consolidate translations into a single JSON file
    Merged separate language JSON files into a unified `translations.json` file for improved organization and maintainability. Updated the language loading script to accommodate the new structure and added error handling for missing translations.

--- home/static/home/scripts/translate.js
+++ home/static/home/scripts/translate.js
@@ -13,37 +13,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Function to load language
     const loadLanguage = (lang) => {
-        fetch(`/static/home/json/${lang}.json`)
+        fetch(`/static/home/json/translations.json`)
             .then(response => response.json())
             .then(data => {
+                const translations = data[lang];
+                if (!translations) {
+                    console.error(`Translations for language "${lang}" not found.`);
+                    return;
+                }
+
                 // Header content
-                document.getElementById('lang_genome_decoding').textContent = data.genomeDecoding;
-                document.getElementById('lang_about_us').textContent = data.aboutUs;
-                document.getElementById('lang_mission').textContent = data.mission;
-                document.getElementById('lang_team').textContent = data.team;
-                document.getElementById('lang_species').textContent = data.species;
-                document.getElementById('lang_downloads').textContent = data.downloads;
-                document.getElementById('lang_reports').textContent = data.reports;
-                document.getElementById('lang_datasets').textContent = data.datasets;
-                document.getElementById('lang_software').textContent = data.software;
+                document.getElementById('lang_genome_decoding').textContent = translations.genomeDecoding;
+                document.getElementById('lang_about_us').textContent = translations.aboutUs;
+                document.getElementById('lang_mission').textContent = translations.mission;
+                document.getElementById('lang_team').textContent = translations.team;
+                document.getElementById('lang_species').textContent = translations.species;
+                document.getElementById('lang_downloads').textContent = translations.downloads;
+                document.getElementById('lang_reports').textContent = translations.reports;
+                document.getElementById('lang_datasets').textContent = translations.datasets;
+                document.getElementById('lang_software').textContent = translations.software;
+
                 // Main content
-                document.getElementById('team-frontshow-text-header').querySelector('h3').textContent = data.theTeam;
-                document.getElementById('team-frontshow-text').querySelector('p').textContent = data.theTeamDescription;
-                document.getElementById('team-frontshow-button').textContent = data.theTeamLearnMore;
+                document.getElementById('team-frontshow-text-header').querySelector('h3').textContent = translations.theTeam;
+                document.getElementById('team-frontshow-text').querySelector('p').textContent = translations.theTeamDescription;
+                document.getElementById('team-frontshow-button').textContent = translations.theTeamLearnMore;
 
                 // Load leish intro content
                 loadLeishInfoCards(lang);
 
             })
-            .catch(error => console.error('Error loading language:', error));
+            .catch(error => console.error('Error loading language file:', error));
 
         // Change flag
         const flagSrc = languageDropdown.querySelector(`option[value="${lang}"]`).dataset.flag;
         selectedFlag.src = flagSrc;
     };
 
-    // Load language from localStorage if exists
-    const savedLanguage = localStorage.getItem('selectedLanguage') || 'eng';
+    // Load language from localStorage if it exists
+    const savedLanguage = localStorage.getItem('selectedLanguage') || 'en';
     languageDropdown.value = savedLanguage;
     loadLanguage(savedLanguage);
 
