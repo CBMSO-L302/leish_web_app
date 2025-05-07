dfb756b9b28c452b77ddebd04c4a85eae15bbdb9
rfpacheco <ronnyfph@gmail.com>
2025-05-07 15:14:14 +0200

    Refactor team page to dynamically load data from JSON.
    Replaced hardcoded member details in the team template with dynamic data populated from a JSON file. Updated `views.py` to read and pass JSON data for current members, former members, and students to the template. This approach improves maintainability and simplifies future updates to team information.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+from django.conf import settings
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
@@ -8,3 +11,13 @@ class OnDevelopment(TemplateView):
 
 class TeamPageView(TemplateView):
     template_name = "home/team.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        json_path = Path(settings.BASE_DIR) / "home/static/home/json/team-html.json"
+        with open(json_path, "r", encoding="utf-8") as json_file:
+            data = json.load(json_file)
+        context["current_members"] = data.get("current_members", [])
+        context["former_members"] = data.get("former_members", [])
+        context["former_students"] = data.get("former_students", [])
+        return context
